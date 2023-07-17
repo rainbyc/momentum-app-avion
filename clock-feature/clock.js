1e28dfe6d3661bce683473e713edae5c051de3ab
rainbyc <work.rainier@gmail.com>
2023-07-17 23:45:24 +0800

    Add the code for the clock.js file and code for the clock.css

---
+++ clock-feature/clock.js
@@ -0,0 +1,80 @@
+
+function displayTime() {
+    let dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let period = document.getElementById('period');
+
+    if(hrs >= 12) {
+        period.innerHTML = 'PM'
+    } else {
+        period.innerHTML = 'AM'
+    }
+
+    if(hrs > 12) {
+        hrs = hrs - 12;
+    }
+
+    if(hrs === 0) {
+        hrs = 12;
+    }
+
+    if(min < 10) {
+        min = '0' + min;
+    }
+
+    if(sec < 10) {
+        sec = '0' + sec;
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 500);
+
+//drag clock
+
+dragElement(document.getElementById("clock"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "clock-cont")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "clock-cont").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
