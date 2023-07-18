d06325fad2de56ea9d3ab70993dcce296ff9ffb5
rainbyc <work.rainier@gmail.com>
2023-07-18 19:46:37 +0800

    Update the style of the greeting feature

--- greeting-feature/greeting.js
+++ greeting-feature/greeting.js
@@ -1,19 +1,27 @@
-function showGreeting() {
-    let nameInput = document.getElementById("nameInput");
-    let greeting = document.getElementById("greeting");
+function showGreeting(event) {
+    if (event.keyCode === 13) {
+      let nameInput = document.getElementById("nameInput");
+      let greeting = document.getElementById("greeting");
   
-    let name = nameInput.value;
-    let currentTime = new Date();
-    let currentHour = currentTime.getHours();
-    let greetingMessage = "";
+      let name = nameInput.value;
+      let currentTime = new Date();
+      let currentHour = currentTime.getHours();
+      let greetingMessage = "";
   
-    if (currentHour >= 0 && currentHour < 12) {
-      greetingMessage = "Good morning";
-    } else {
-      greetingMessage = "Good evening";
+      if (currentHour >= 0 && currentHour < 12) {
+        greetingMessage = "Good morning";
+      } else {
+        greetingMessage = "Good evening";
+      }
+  
+      greeting.textContent = greetingMessage + ", " + name + "!";
+      nameInput.value = "";
     }
+}
   
-    greeting.textContent = greetingMessage + ", " + name + "!";
-    nameInput.value = "";
-  }
+function checkEnter(event) {
+    if (event.keyCode === 13) {
+      showGreeting(event);
+    }
+}
   
